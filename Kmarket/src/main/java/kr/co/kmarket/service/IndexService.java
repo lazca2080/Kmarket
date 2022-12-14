1bed7a08b034466a6ab5b8b7f634ba932b56dff0
lazca <lazca2080@gmail.com>
2022-12-14 16:26:15 +0900

    add index

--- Kmarket/src/main/java/kr/co/kmarket/service/IndexService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/IndexService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import kr.co.kmarket.dao.IndexDAO;
 import kr.co.kmarket.vo.CategoryVO;
+import kr.co.kmarket.vo.ProductVO;
 
 public enum IndexService {
 	
@@ -15,7 +16,19 @@ public enum IndexService {
 		return dao.selectCate(cate);
 	}
 	
+	public List<CategoryVO> selectCategory() {
+		return dao.selectCategory();
+	}
+	
 	public List<CategoryVO> selectCate1() {
 		return dao.selectCate1();
+	}
+	
+	public List<ProductVO> selectBest1() {
+		return dao.selectBest1();
+	}
+	
+	public List<ProductVO> selectBest() {
+		return dao.selectBest();
 	}
 }
