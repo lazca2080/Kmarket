456075c821a47dfa84238fd36ee6c630adae0d2d
aowlrxm <aowlrxm15>
2022-12-16 19:41:02 +0900

    2022-12-16

--- Kmarket/src/main/java/kr/co/kmarket/db/AdminSql.java
+++ Kmarket/src/main/java/kr/co/kmarket/db/AdminSql.java
@@ -2,9 +2,10 @@ package kr.co.kmarket.db;
 
 public class AdminSql {
 
-	public static final String selectProduct = "SELECT * FROM `km_product`";
+	public static final String selectProduct = "SELECT * FROM `km_product` ";
 	
-	public static final String selectProductss = "SELECT * FROM `km_product` WHERE `seller`=?";
+	public static final String selectProductss = "SELECT * FROM `km_product` WHERE `seller`=? "
+			 										+ "order by `prodNo` desc LIMIT ?, 10";
 
 	public static final String selectProductKeyword = "SELECT `prodNo`,`prodName`,`company` FROM `km_product` "
 													+ "WHERE `seller` = ? and "
@@ -14,4 +15,9 @@ public class AdminSql {
 													+ "ORDER BY `prodNo` DESC "
 													+ "LIMIT 0,10";
 
+	public static final String SELECT_ADMIN_LIST = "SELECT * FROM `km_product` WHERE `seller`=? "
+												 + "order by `prodNo` desc LIMIT ?, 10";
+	
+	public static final String SELECT_COUNT_TOTAL = "SELECT COUNT(`prodNo`) FROM `km_product` "
+												  + "where `seller`=? ";
 }
--- Kmarket/src/main/java/kr/co/kmarket/service/AdminService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/AdminService.java
@@ -2,6 +2,9 @@ package kr.co.kmarket.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import kr.co.kmarket.dao.AdminDAO;
 import kr.co.kmarket.dao.ProductDAO;
 import kr.co.kmarket.vo.MemberVO;
@@ -10,20 +13,78 @@ import kr.co.kmarket.vo.ProductVO;
 public enum AdminService {
 	
 	INSTANCE;
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	private AdminDAO dao;
 	private AdminService() { dao = new AdminDAO(); }
 	
 	// product 상품 리스트 불러오기
-	public ProductVO selectProducts() {
-		return dao.selectProducts();
+	public ProductVO selectProducts(int level) {
+		return dao.selectProducts(level);
 	}
 	
 	
+	// 판매자(관리자)의 상품목록만 조회
+	public List<ProductVO> selectProductss(int limitdstart,String uid) {
+		return dao.selectProductss(limitdstart,uid);
+	}
 	
-	public List<ProductVO> selectProductss(String uid) {
-		return dao.selectProductss(uid);
+	public int selectCountTotal(String seller) {
+		return dao.selectCountTotal(seller);
+	}
+	
+	public List<ProductVO> selectAdminList(int limiteStart, String seller) {
+		return dao.selectAdminList(seller, limiteStart);
 	}
 
+	// 서비스 로직
+	
+	//페이지 그룹 계산
+	public int[] getpageGroupNum(int currentPage, int lastPageNum) {
+		int pageGroupCurrent = (int)Math.ceil(currentPage / 10.0);
+		int pageGroupStart = (pageGroupCurrent - 1)  * 10 + 1;
+		int pageGroupEnd = pageGroupCurrent * 10;
+		
+		if(pageGroupEnd > lastPageNum) {
+			pageGroupEnd = lastPageNum;
+		}
+		
+		int[] result = {pageGroupStart, pageGroupEnd};
+		
+		return result;
+	}
+	
+	//시작되는 페이지그룹번호
+	public int getPageStartNum(int total, int currentPage) {
+		int start = (currentPage - 1 ) * 10;
+		return total - start;
+	}
+	
+	//현재 페이지의 값(첫화면 표시를 위해 1)
+	public int getCureentPage(String pg) {
+		int currentPage = 1;
+		
+		if(pg != null) {
+			currentPage = Integer.parseInt(pg);
+		}
+		return currentPage;
+	}
+	
+	//시작값
+	public int getStartNum(int currentPage){
+		return(currentPage -1 ) * 10;
+	}
+	
+	//마지막페이지 값
+	public int getLastPageNum(int total){
+		int lastPageNum = 0;
+		
+		if(total % 10 == 0) {
+			lastPageNum = total / 10;
+		}else {
+			lastPageNum = total / 10 + 1;
+		}
+		return lastPageNum;
+	}
 	
 	
 	/*
