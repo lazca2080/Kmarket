18bd0b6c83ff8733d14412a67b907fc4d3cb6935
lazca9527 <lazca9527@gmail.com>
2022-12-21 13:39:33 +0900

    2022/12/21

--- Kmarket/src/main/java/kr/co/kmarket/controller/product/CompleteController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/product/CompleteController.java
@@ -1,6 +1,8 @@
 package kr.co.kmarket.controller.product;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.co.kmarket.service.IndexService;
+import kr.co.kmarket.vo.CompleteVO;
 
 @WebServlet("/product/complete.do")
 public class CompleteController extends HttpServlet{
@@ -34,6 +37,45 @@ public class CompleteController extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		String now = sdf.format(new Date());
+		
+		String uid = req.getParameter("uid");
+		String ordNo        = now+uid;
+		String ordCount     = req.getParameter("ordCount");
+		String ordPrice     = req.getParameter("ordPrice");
+		String ordDiscount  = req.getParameter("ordDiscount");
+		String ordDelivery  = req.getParameter("ordDelivery");
+		String ordsavePoint = req.getParameter("ordsavePoint");
+		String ordusedPoint = req.getParameter("ordusedPoint");
+		String ordTotPrice  = req.getParameter("ordTotPrice");
+		String orderer      = req.getParameter("orderer");
+		String hp 			= req.getParameter("hp");
+		String zip 			= req.getParameter("zip");
+		String addr1 		= req.getParameter("addr1");
+		String addr2 		= req.getParameter("addr2");
+		String payment 		= req.getParameter("payment");
+		
+		CompleteVO vo = new CompleteVO();
+		vo.setOrdNo(ordNo);
+		vo.setOrdUid(uid);
+		vo.setOrdCount(ordCount);
+		vo.setOrdPrice(ordPrice);
+		vo.setOrdDiscount(ordDiscount);
+		vo.setOrdDelivery(ordDelivery);
+		vo.setOrdSavePoint(ordsavePoint);
+		vo.setOrdUsedPoint(ordusedPoint);
+		vo.setOrdTotPrice(ordTotPrice);
+		vo.setOrdRecipHp(hp);
+		vo.setOrdRecipName(orderer);
+		vo.setOrdRecipZip(zip);
+		vo.setOrdRecipAddr1(addr1);
+		vo.setOrdRecipAddr2(addr2);
+		vo.setOrdPayment(payment);
+		vo.setOrdCompelete("1");
+		
+		
 	}
 
 }
